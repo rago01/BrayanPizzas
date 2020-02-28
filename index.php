330b94f723e95501f8a80709e03fc95ca15b2280
Oscar García <ragogarcia1@gmail.com>
2020-02-28 11:40:59 -0500

    Organizando los archivos por carpetas

--- index.php
+++ index.php
@@ -1,22 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<head>
     <?php include('conexion.php')?>
     <link rel="stylesheet" href="estilos/index.css">
     <title>Brayans Pizzas</title>
 </head>
-<body class="container">
-    <section class=" text-center">
-        <article class="row justify-content-md-center align-items-center">
-            <div class="col-lg-6">
-              <form class="" action="loginCliente.php" method="post">
-                <label class="h2">Número de celular:</label>
-                <input type="number" class="form-control" name="celular">
-                <p class="h2"><input type="submit" class="btn btn-primary" value="Ingresar">  ó <a href="register.php">Registrarse</a> </p>
-              </form>
-            </div>
-        </article>
+<body>
+    <section class="limiter">
+      <header class="container">
+        <div class="col-12 text-center">
+          <img  src="img/logo.png" class="logo" alt="">
+        </div>
+      </header>
+      <div class="container-login col-12 text-center">
+        <form action="loginCliente.php" method="post">
+          <label class="h2">Número de celular:</label>
+          <input type="number" class="form-control" name="celular">
+          <p class="h2"><input type="submit" class="btn btn-danger" value="Ingresar">  ó <a href="register.php">Registrarse</a> </p> <hr>
+          <p class="h4 "> <a class="warning" href="loginAdmin.php">Ingresar como Administrador ó Cajero</a>  </p>
+        </form>
+      </div>
     </section>
 </body>
 </html>
